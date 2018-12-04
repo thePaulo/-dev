7071ac4df7dcf0238ac39f976c6275f09d18ace5
Paulo Sousa <the_paulog@hotmail.com>
2018-12-04 17:21:50 -0200

    updating music activity to the newest version

--- Voice_project/MusicActivity.java
+++ Voice_project/MusicActivity.java
@@ -1,20 +1,36 @@
 package com.example.paulov.voice_project;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.media.MediaPlayer;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MusicActivity extends AppCompatActivity {
     MediaPlayer mp3 = new MediaPlayer();
+    //ArrayList<String> animes;
+    //ArrayList<String> musics;
     String[] animes;
     String[] musics;
     TypedArray images;
@@ -26,12 +42,14 @@ public class MusicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
 
-        animes = getResources().getStringArray(R.array.animes);//lista de animes
+        //animes = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.animes)));//lista de animes
+        //musics = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.animes)));//lista de animes
+        //images = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.animes)));//lista de animes
+        animes = getResources().getStringArray(R.array.animes);//lista de musicas
         musics = getResources().getStringArray(R.array.musics);//lista de musicas
-        //images = getResources().getIntArray(R.array.animes);//lista de imagens
-        images = getResources().obtainTypedArray(R.array.images);
+        images = getResources().obtainTypedArray(R.array.images);//lista de imagens
 
-        //parte referente ao strings.h
+        //parte referente ao strings.h para autocomplete
         auto = findViewById(R.id.edt_anime);
 
         ArrayAdapter<CharSequence> adapter =
@@ -40,36 +58,128 @@ public class MusicActivity extends AppCompatActivity {
         auto.setAdapter(adapter);
         //fim da parte do strings.h
 
-
+        //parte referente a tocar a musica
         Random random = new Random();
-
         index = random.nextInt(animes.length);
         try{
+            //mp3.setDataSource("https://api-2.datmusic.xyz/links/6d891599686e216eb28a66c84b81d55d/Noragami%20Aragoto%20-%20Official%20Opening%20-%20Kyouran%20Hey%20Kids.mp3");
+            //mp3.setDataSource("https://api-2.datmusic.xyz/links/268e6efa393c1d347c1e0ae63d109ec4/Nightcore%20-%20I%20Like%20It%20Loud%20Cash%20Cash%20Cover.mp3");
             mp3.setDataSource(musics[index]);
             mp3.prepare();
 
         }catch (Exception e){}
 
         mp3.start();
+        //fim da parte de tocar musica
+        notification_stuff();
+
+    }
+
+    public void notification_stuff(){
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.star_big_off)
+                .setContentTitle("Now playing...")
+                .setContentText("??");
+
+        Icon icon = Icon.createWithResource(this,android.R.drawable.star_big_on);
+        Intent intent = new Intent(getApplicationContext(),NotificationReceiver.class);
+        intent.setAction(NotificationReceiver.ACTION_BUTTON_1);
+        intent.putExtra("action",animes[index]);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),50,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        //sendBroadcast(intent);
+        //sendOrderedBroadcast(intent,null);
+
+        Notification.Action action = new Notification.Action.Builder(icon,"Show song",pendingIntent).build();
+        builder.addAction(action);
+
+        Notification notification= builder.build();
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(50,notification);
+
     }
 
 
     public void submit(View view) {
 
+        TextView name = (TextView)findViewById(R.id.type_id);
+
         if(auto.getText().toString().equals(animes[index])){
             Toast.makeText(this,"SEIKAI !!",Toast.LENGTH_SHORT).show();
             //getSupportFragmentManager().findFragmentById(R.id.fragment_id).getView().setBackgroundColor(R.drawable.mob_psycho);
-            Fragment f = (ImageFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
-            ((ImageFragment) f).setImage(images.getResourceId(index,-1));
+            /*Fragment f = (ImageFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
+            ((ImageFragment) f).setImage(images.getResourceId(index,-1));*/
+            name.setTextColor(Color.GREEN);
+
+
         }else{
-            Toast.makeText(this,"WARUI KOTAE :(",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"CHIGAU :(",Toast.LENGTH_SHORT).show();
+            name.setTextColor(Color.RED);
         }
+
+        Fragment f = (ImageFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_id);
+        ((ImageFragment) f).setImage(images.getResourceId(index,-1));
+
+        TextView songName = (TextView)findViewById(R.id.song_id);
+        //songName.setText("Now Playing : ");//+animes[index]);
+        songName.setText("Last song was : ");//+animes[index]);
+        //TextView name = (TextView)findViewById(R.id.type_id);
+        name.setText(animes[index] + "\nGUESS THE NEXT ANIME SONG!");
+
+        //auto.setVisibility(View.INVISIBLE);
+        auto.setText("");
+        //auto.setFocusable(false);
+        //auto.setFocusable(true);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);//Escondendo o teclado do google
+
+
+
+
+        //mp3.stop();
+        mp3.pause();
+        //mp3.seekTo(0);
+        mp3 = new MediaPlayer();
+
+        //parte referente a tocar a musica
+        Random random = new Random();
+        index = random.nextInt(animes.length);
+        try{
+            mp3.setDataSource(musics[index]);
+            //mp3.setOnPreparedListener(this);
+            //mp3.prepareAsync();
+            mp3.prepare();
+
+        }catch (Exception e){}
+
+        //mp3.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                SystemClock.sleep(2000);
+
+                mp3.start();
+
+                //fim da parte de tocar musica
+            }
+        }).start();
+
+
+        notification_stuff();
 
     }
 
     public void stop(View view) {
+        parar();
+    }
+    public void parar(){
         mp3.stop();
         mp3.release();
 
+        NotificationManager nm = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.cancel(50);
     }
+
+
 }
